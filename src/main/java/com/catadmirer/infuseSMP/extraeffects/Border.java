f181bd24df5430ee90d2b19874c0bfaba36c4bb0
TurboJax <turbojax07@gmail.com>
2026-08-08 20:19:29 -0400

    Editing lore comment in border effect

--- src/main/java/com/catadmirer/infuseSMP/extraeffects/Border.java
+++ src/main/java/com/catadmirer/infuseSMP/extraeffects/Border.java
@@ -2,22 +2,20 @@ package com.catadmirer.infuseSMP.extraeffects;
 
 public class Border {
     //TODO hopefully soon
-    /**
-     * Border Lore:
-     * <red><b>ᴘᴀꜱꜱɪᴠᴇ ᴇꜰꜰᴇᴄᴛꜱ:
-     * <red>🚫 <dark_gray>
-     * <red>🚫 <dark_gray>
-     * <red>🚫 <dark_gray>
-     * <red>🚫 <dark_gray>
-     * <red>🚫 <dark_gray>
-     * <dark_gray>
-     * <red><b>ꜱᴘᴀʀᴋ ᴇꜰꜰᴇᴄᴛꜱ:
-     * <red>🚫 <dark_gray>Make a 16x16 border around you, trapping your enemies.
-     * <red>🚫 <dark_gray>This area does not move when it is summoned.
-     * <red>🚫 <dark_gray>Teammates can enter/exit the border freely.
-     * <red>🚫 <dark_gray>
-     * <dark_gray>
-     * <dark_aqua>ᴅᴜʀᴀᴛɪᴏɴ: 30s
-     * <dark_aqua>ᴄᴏᴏʟᴅᴏᴡɴ: 30s
-     */
+/*
+<red><b>ᴘᴀꜱꜱɪᴠᴇ ᴇꜰꜰᴇᴄᴛꜱ:
+<red>🚫 <dark_gray>
+<red>🚫 <dark_gray>
+<red>🚫 <dark_gray>
+<red>🚫 <dark_gray>
+<red>🚫 <dark_gray>After you are hit 10 times, your attacker is frozen in place.
+<dark_gray>
+<red><b>ꜱᴘᴀʀᴋ ᴇꜰꜰᴇᴄᴛꜱ:
+<red>🚫 <dark_gray>Make a 16x16 border around you, trapping your enemies.
+<red>🚫 <dark_gray>This area does not move when it is summoned.
+<red>🚫 <dark_gray>Teammates can enter/exit the border freely.
+<dark_gray>
+<dark_aqua>ᴅᴜʀᴀᴛɪᴏɴ: 30s
+<dark_aqua>ᴄᴏᴏʟᴅᴏᴡɴ: 30s
+ */
 }
